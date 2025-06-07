2c372ab43f841b1e8129f7ce0cc36f0ca43e2ffd
Allanzin178 <allan.miron@hotmail.com>
2025-06-07 13:17:57 -0300

    Adicionando tailwindcss ao projeto

--- petshop-mundoanimal/src/App.jsx
+++ petshop-mundoanimal/src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   return (
     // Conteudo aqui
     <div>
-      <h1>Bem vindo!</h1>
+      <h1 className='text-5xl font-bold underline hover:cursor-pointer'>Bem vindo!</h1>
     </div>
   )
 }
