66e772d2a83d222d486db938add167cd9f0ebca0
Allanzin178 <allan.miron@hotmail.com>
2025-06-08 20:16:38 -0300

    delete

--- petshop-mundoanimal-api/src/routes/pet.route.js
+++ petshop-mundoanimal-api/src/routes/pet.route.js
@@ -1,5 +1,5 @@
 import { Router } from 'express'
-import { findAllPets, registerPet, findPet, findUserPets, updatePet } from '../controllers/pet.controller.js'
+import { findAllPets, registerPet, findPet, findUserPets, updatePet, deletePet } from '../controllers/pet.controller.js'
 import { authMiddleware, roleMiddleware } from '../middlewares/auth.middleware.js'
 import { validId, validPet } from '../middlewares/global.middlewares.js'
 
@@ -11,7 +11,6 @@ router.get('/', authMiddleware, roleMiddleware, findAllPets)
 router.get('/byUser/', authMiddleware, findUserPets)
 router.get('/:id', validId, validPet, authMiddleware, findPet)
 router.put('/:id', validId, validPet, authMiddleware, updatePet)
-
-
+router.delete('/:id', validId, validPet, authMiddleware, deletePet)
 
 export default router
--- petshop-mundoanimal-api/src/routes/user.routes.js
+++ petshop-mundoanimal-api/src/routes/user.routes.js
@@ -1,4 +1,4 @@
-import { findAllUsers, registerUser, findUser, updateUser, findUserByToken } from "../controllers/user.controller.js";
+import { findAllUsers, registerUser, findUser, updateUser, findUserByToken, deleteUser } from "../controllers/user.controller.js";
 import { authMiddleware, roleMiddleware } from "../middlewares/auth.middleware.js";
 import { validId, validUser } from "../middlewares/global.middlewares.js";
 import { Router } from "express";
@@ -9,5 +9,6 @@ router.get('/', authMiddleware, roleMiddleware, findAllUsers)
 router.get('/findUserByTkn', authMiddleware, findUserByToken)
 router.get('/:id', validId, validUser, findUser)
 router.put("/:id", validId, validUser, authMiddleware, updateUser)
+router.delete('/:id', validId, validUser, authMiddleware, deleteUser)
 
 export default router
