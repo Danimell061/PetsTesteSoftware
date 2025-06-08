8ddbd2918841e277844481a004487733c580e7ee
Allanzin178 <allan.miron@hotmail.com>
2025-06-08 18:18:25 -0300

    Verificacao de cargos

--- petshop-mundoanimal-api/src/routes/user.routes.js
+++ petshop-mundoanimal-api/src/routes/user.routes.js
@@ -1,4 +1,4 @@
-import { findAllUsers, registerUser, findUser, updateUser } from "../controllers/user.controller.js";
+import { findAllUsers, registerUser, findUser, updateUser, findUserByToken } from "../controllers/user.controller.js";
 import { authMiddleware, roleMiddleware } from "../middlewares/auth.middleware.js";
 import { validId, validUser } from "../middlewares/global.middlewares.js";
 import { Router } from "express";
@@ -6,6 +6,7 @@ const router = Router()
 
 router.post('/', registerUser)
 router.get('/', authMiddleware, roleMiddleware, findAllUsers)
+router.get('/findUserByTkn', authMiddleware, findUserByToken)
 router.get('/:id', validId, validUser, findUser)
 router.put("/:id", validId, validUser, authMiddleware, updateUser)
 
--- petshop-mundoanimal/src/pages/Funcionário/Home/HomePageFuncionario.jsx
+++ petshop-mundoanimal/src/pages/Funcionário/Home/HomePageFuncionario.jsx
@@ -1,9 +1,39 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Outlet, useNavigate } from 'react-router-dom';
 import Sidebar from '../components/Sidebar/Sidebar';
+import Cookies from 'js-cookie';
 import './HomePageFuncionario.css';
+import { getUserLogged } from '../../../services/userServices';
 
 export default function HomePageFuncionario() {
+  const navigate = useNavigate()
+
+  useEffect(()=>{
+    const verificarPermissao = async () =>{
+    try{
+      const token = Cookies.get('token')
+      if(!token){
+        console.log("Sem login")
+        navigate('/')
+        return
+      }
+      const response = await getUserLogged()
+      if(response.status !== 200){
+        navigate('/')
+        return
+      }
+      const userRole = response.data.role
+      if (userRole !== 'admin' && userRole !== 'funcionario') {
+        console.log('Você não tem permissão para acessar esta área');
+        navigate('/cliente');
+      }
+
+    }catch(error){
+      console.log('Erro ao verificar permissões: ', error)
+      navigate('/')
+    }}
+    verificarPermissao()
+  },[ navigate ])
   return (
     <div className="funcionario-container">
       <Sidebar />
